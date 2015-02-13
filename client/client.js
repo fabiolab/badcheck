ea95d7e75bb26b0df0e89771d59a419bcfdbcfc5
Fabrice Depaulis <fdepaulis@pagesjaunes.fr>
2015-02-13 17:37:10 +0100

    First true version

--- client/client.js
+++ client/client.js
@@ -1,11 +1,40 @@
+
+Meteor.subscribe('players');
+
 Template.playerlist.helpers({
-  players: function() {
-    return playersCol.find();
-  }
+    players: function() {
+        return playersCol.find();
+    }
 })
 
 Template.playerlist.events({
-  click: function(){
-    this.here = true;
-  }
+    click: function(){    
+        new_here = !this.here;
+        Meteor.call('updatePresence',this._id,new_here);
+    }
+})
+
+Template.upload.events({
+    "change #files": function (e) {
+
+        Meteor.call('removeAllPlayers');
+
+        var files = e.target.files || e.dataTransfer.files;
+        for (var i = 0, file; file = files[i]; i++) {
+            if (file.type.indexOf("text") == 0) {
+                var reader = new FileReader();
+                reader.onloadend = function (e) {
+                    var text = e.target.result;
+                    var lines = text.split("\n");
+                    for(var i = 0; i<lines.length; i++){
+                        var fields = lines[i].split(";");
+                        doc = {'name':fields[0],'here':(fields[1].toLowerCase() == "true")};
+                        // playersCol.insert(doc);
+                        Meteor.call('insertPlayer',doc);
+                    }
+                }
+                reader.readAsText(file);
+            }
+        }
+    }
 })
--- server/server.js
+++ server/server.js
@@ -1,3 +1,34 @@
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Meteor.startup(function() {
+    
+    Meteor.publish(
+        'players', function(){
+            return playersCol.find()
+        }
+    );
+
+    return Meteor.methods({
+        
+        /**
+         * Removes all players from the db
+         */ 
+        removeAllPlayers: function() {
+            return playersCol.remove({});
+        },
+
+        /**
+         * Updates a player presence
+         * @pId         The player ID
+         * @pPresence   A boolean (true for here, false for not here)
+         */
+        updatePresence: function(pId, pPresence){
+            playersCol.update({'_id':pId},{'$set':{'here':pPresence}},{upsert:false})
+        },
+
+        /**
+         * Insert a player into the collection
+         */
+         insertPlayer: function(pPlayer){
+            return playersCol.insert(pPlayer);
+         }
+    });
+});
